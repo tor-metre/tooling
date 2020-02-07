98acd859d836560d14d756670f8a930802502ece
Dennis Jackson <ommitted>
2020-02-07 10:25:56 +0000

    Fix URL checking for WPT Server

--- utility/configuration.py
+++ utility/configuration.py
@@ -47,7 +47,7 @@ def validate_config(config):
             logging.critical(f"Required configuration key f{k} is missing.")
             success = False
     if WPT_SERVER_URL_ENTRY in config:  # TODO - Check the actual HEAD response is correct
-        condition, result = url_is_alive(config[WPT_SERVER_URL_ENTRY])
+        condition, result = url_is_alive(f"http://{config[WPT_SERVER_URL_ENTRY]}")
         if not condition:
             logging.critical(f"Could not  reach wpt server at {config[WPT_SERVER_URL_ENTRY]}. Error: {result.reason}")
             success = False
